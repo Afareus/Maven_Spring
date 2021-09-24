fcd1eb63702e577bd3ea551d4d60a75585a5b3f8
Afareus <afareus89@gmail.com>
2021-09-24 12:30:36 +0200

    Funkční verze vzracejicí JSON

--- src/main/java/cz/vsb/kovacs/conector/weatherStrackConector.java
+++ src/main/java/cz/vsb/kovacs/conector/weatherStrackConector.java
@@ -1,5 +1,12 @@
 package cz.vsb.kovacs.conector;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
 public class weatherStrackConector {
 
 	private static String baseUrl = "http://api.weatherstack.com/";
@@ -7,4 +14,16 @@ public class weatherStrackConector {
 	private static String apiKey = "da223c0631c80400f3e7ed08a1a33fc9";
 	private static String url = baseUrl+urlParams+apiKey+"&query=";
 	
+	
+	public String getWeatherForCity(String city) {
+		 URI uri = null;
+		 RestTemplate template = new RestTemplate(); 
+		 try {
+			 uri = new URI(url + city); 
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		 ResponseEntity<String> response = template.getForEntity(uri, String.class);
+		 return response.getBody();
+	}
 }
--- src/main/java/cz/vsb/kovacs/controller/WeatherController.java
+++ src/main/java/cz/vsb/kovacs/controller/WeatherController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cz.vsb.kovacs.conector.weatherStrackConector;
+
 @RestController
 public class WeatherController {
 	
@@ -14,7 +16,8 @@ public class WeatherController {
 	
 	@RequestMapping("/weather/{city}")                               // to co je ve složených závorkách je proměnná 
 	public String getWeatherForCity(@PathVariable String city) {
-		return "Pocasi pro mesto " + city;
+		weatherStrackConector con = new weatherStrackConector();	
+		return  con.getWeatherForCity(city);
 	}
 
 
