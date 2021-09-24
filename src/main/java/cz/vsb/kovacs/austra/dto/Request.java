21dca5004c0113a7a9679c0defea3cde0035c981
Afareus <afareus89@gmail.com>
2021-09-24 13:58:06 +0200

    přilal jsem třídy z JSON

--- src/main/java/cz/vsb/kovacs/austra/dto/Request.java
+++ src/main/java/cz/vsb/kovacs/austra/dto/Request.java
@@ -1,8 +1,33 @@
 package cz.vsb.kovacs.austra.dto;
 
 public class Request {
-    public String type;
-    public String query;
-    public String language;
-    public String unit;
+    private String type;
+    private String query;
+    private String language;
+    private String unit;
+    
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getQuery() {
+		return query;
+	}
+	public void setQuery(String query) {
+		this.query = query;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
 }
--- src/main/java/cz/vsb/kovacs/austra/dto/WeatherstackDto.java
+++ src/main/java/cz/vsb/kovacs/austra/dto/WeatherstackDto.java
@@ -2,8 +2,27 @@ package cz.vsb.kovacs.austra.dto;
 
 public class WeatherstackDto {
 	
-    public Request request;
-    public Location location;
-    public Current current;
+	private Request request;
+	private Location location;
+	private Current current;
+
+	public Request getRequest() {
+		return request;
+	}
+	public void setRequest(Request request) {
+		this.request = request;
+	}
+	public Location getLocation() {
+		return location;
+	}
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	public Current getCurrent() {
+		return current;
+	}
+	public void setCurrent(Current current) {
+		this.current = current;
+	}
 	
 }
