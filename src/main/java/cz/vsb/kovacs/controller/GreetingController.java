ddaa476ca0a9b50c86cc3a018ff91e4c9d4c7a84
Afareus <afareus89@gmail.com>
2021-09-24 09:32:45 +0200

    První commit

---
+++ src/main/java/cz/vsb/kovacs/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package cz.vsb.kovacs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	@RequestMapping("/ahoj")
+	String greeting() {
+		return "ahoj!";
+	}
+	
+}
+
